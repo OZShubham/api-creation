5d7175c309969619cdc28fbd95d0f3b3fc6e1c5f
ankur verma <verma.ankur1729@gmail.com>
2023-05-09 22:03:09 +0530

    modified

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
-url = 'https://login-zlf25kyp3a-el.a.run.app/api/login'
+url = 'https://login-rahul-zlf25kyp3a-el.a.run.app/api/login'
 data = {
     
     'email': 'lokesh@gmail.com',
