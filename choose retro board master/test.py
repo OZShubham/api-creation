1700a0826b96e9ce011ba312e9f2d8a3f26eed45
ankur verma <verma.ankur1729@gmail.com>
2023-05-31 13:08:40 +0530

    change comment

--- choose retro board master/test.py
+++ choose retro board master/test.py
@@ -1,9 +1,9 @@
 import requests
 
-# Define the API endpoint URL
+# API endpoint URL
 url = 'https://retro-master-zlf25kyp3a-el.a.run.app/api/choose_retro_board_master'
 
-# Define the request payload with email and poker_board_id
+#  the request payload with email and poker_board_id
 payload = {'email': 'ankur@gmail.com', 'poker_board_id': '031413e74f728f906955a6a3ce8d5424'}
 
 # Send a POST request to the endpoint
